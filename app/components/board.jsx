07312736767d6020ad88d4d3a908b1e3bcc193e2
“Xiugapurin” <jason32567810@gmail.com>
2024-12-22 21:09:16 +0800

    modify web socket method

--- app/components/board.jsx
+++ app/components/board.jsx
@@ -9,39 +9,6 @@ import { Award, Trophy, UsersRound } from "lucide-react";
 import { UserAuth } from "../context/AuthContext";
 import { Button } from "@/components/ui/button";
 
-const SCORE = [
-  { group_id: "1", group_score: 1, group_name: "awards" },
-  { group_id: "2", group_score: 2, group_name: "trophies" },
-  { group_id: "3", group_score: 3, group_name: "users" },
-  { group_id: "4", group_score: 4, group_name: "awards" },
-  { group_id: "5", group_score: 5, group_name: "trophies" },
-  { group_id: "6", group_score: 6, group_name: "users" },
-  { group_id: "7", group_score: 7, group_name: "awards" },
-  { group_id: "8", group_score: 8, group_name: "trophies" },
-  { group_id: "9", group_score: 9, group_name: "users" },
-  { group_id: "10", group_score: 10, group_name: "awards" },
-  { group_id: "11", group_score: 11, group_name: "trophies" },
-  { group_id: "12", group_score: 12, group_name: "users" },
-  { group_id: "13", group_score: 13, group_name: "awards" },
-  { group_id: "14", group_score: 14, group_name: "trophies" },
-  { group_id: "15", group_score: 15, group_name: "users" },
-  { group_id: "16", group_score: 16, group_name: "awards" },
-  { group_id: "17", group_score: 17, group_name: "trophies" },
-  { group_id: "18", group_score: 18, group_name: "users" },
-  { group_id: "19", group_score: 19, group_name: "awards" },
-  { group_id: "20", group_score: 20, group_name: "trophies" },
-  { group_id: "21", group_score: 21, group_name: "users" },
-  { group_id: "22", group_score: 22, group_name: "awards" },
-  { group_id: "23", group_score: 23, group_name: "trophies" },
-  { group_id: "24", group_score: 24, group_name: "users" },
-  { group_id: "25", group_score: 25, group_name: "awards" },
-  { group_id: "26", group_score: 26, group_name: "trophies" },
-  { group_id: "27", group_score: 27, group_name: "users" },
-  { group_id: "28", group_score: 28, group_name: "awards" },
-  { group_id: "29", group_score: 29, group_name: "trophies" },
-  { group_id: "30", group_score: 30, group_name: "users" },
-];
-
 export default function Board() {
   const { user, logOut } = UserAuth();
   const [scores, setScores] = useState([]);
@@ -62,7 +29,7 @@ export default function Board() {
   }, []);
 
   useEffect(() => {
-    const socket = io(process.env.NEXT_PUBLIC_BACKEND_URI);
+    const socket = io(process.env.NEXT_PUBLIC_BACKEND_URI, { transports: ["websocket"] });
 
     socket.on("score_update", (data) => {
       console.log("Received score update:", data);
